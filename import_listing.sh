f446d0903c0a333fc93e3e6888fef2228fac14a3
GreenSnakeLinux <cyrilfo@yahoo.fr>
2025-04-27 16:48:43 +0200

    First revision

---
+++ import_listing.sh
@@ -0,0 +1,20 @@
+# .BAS file must be saved in CR/LF windows line breaks format
+# and last line of the file must be empty
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4301.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4401.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4402.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4403.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4404.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4701.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4702.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4703.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4704.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4705.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4801.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4802.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4803.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4804.BAS -t 0
+./iDSK ./disk/amstrad_100%.dsk -i ./bas/CPC4901.BAS -t 0
+
+./iDSK ./disk/amstrad_100%.dsk -l
+./cap32 ./disk/amstrad_100%.dsk
